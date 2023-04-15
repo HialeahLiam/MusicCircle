752ba856271fbf3f261e781b3fe3d5d7816c6b37
Liam Idrovo <liamidrovo@gmail.com>
2023-04-14 19:04:26 -0700

    generated spotify web api type definitions

---
+++ src/pages/spotify-connect.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SpotifyConnect() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="mx-auto my-48 text-2xl text-slate-500 md:text-4xl">
+        Connecting to Spotify ...
+      </h1>
+    </div>
+  );
+}
+
+export default SpotifyConnect;
