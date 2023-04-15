554379b6853cea1c7f667717f1f908b5b8887cb4
Liam Idrovo <liamidrovo@gmail.com>
2023-04-14 18:46:18 -0700

    Landing page header and connect button

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+import nextMdx from "@next/mdx";
+const withMDX = nextMdx({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  reactStrictMode: true,
+
+  /**
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+};
+export default withMDX(config);
---
+++ src/components/buttons/SpotifyConnectButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SpotifyConnectButton() {
+  return (
+    <button className=" rounded-full bg-green-500 px-6 py-3 text-white hover:bg-green-400">
+      Connect to Spotify
+    </button>
+  );
+}
+
+export default SpotifyConnectButton;
